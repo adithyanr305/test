8c5e4e7ed7a33fab95377e60e1d5642bd5b625f8
Adithyan R <adithyanr346@gmail.com>
2026-06-21 22:17:41 +0530

    Revert "pizza,burger"
    This reverts commit ac1609174e6f93d2ca49a327b7b9998654ceb972.

--- hungry.py
+++ hungry.py
@@ -1,8 +1,6 @@
 hungry = input("are you hungry")
 if hungry == "yes":
     print("eat samosa")
-    print("eat pizza")
-    print("eat burger")
 elif hungry == "no":
     print("go play football")
 else:
